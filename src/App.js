c454bba165d40b3eb7aa0729155b660d1cb7270a
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-25 19:24:47 -0500

    se agrega darkMode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React, {useState} from 'react';
 import './App.css';
 import { Header } from './components/header/Header';
 
 import { Title } from './components/hero/Title';
 
 function App() {
+
+  //useState
+  const [isDark, setDark] = useState(false);
+
   return (
-    <div>
-      <Header />
+    <div className={`giphos__container ${isDark ? "dark" : ""}`}>
+      <Header isDark={isDark} setDark={setDark} />
       <Title />
     </div>
   );
